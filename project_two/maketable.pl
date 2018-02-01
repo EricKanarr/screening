38f9cb357a726b18fc55da686a75c19fade20677
Eric <erickanarr@gmail.com>
2018-02-01 18:42:33 -0500

    project 2

--- project_two/maketable.pl
+++ project_two/maketable.pl
@@ -4,7 +4,7 @@ use warnings;
 
 use DBI;
 
-my @table_data = ( { first_name => 'Rose',
+my @example_data = ( { first_name => 'Rose',
                      last_name  => 'Tyler',
                      home       => 'Earth' },
                    { first_name => 'Zoe',
@@ -29,4 +29,54 @@ my @table_data = ( { first_name => 'Rose',
                      last_name  => 'Foreman',
                      home       => 'Gallifrey'} );
 
-my $dbh = DBI->connect('dbi:SQLite:dbname=test.sqlite','','',{AutoCommit=>1,RaiseError=>1,PrintError=>0});
+# Connect to DB
+my $dbname = defined $ARGV[0] ? $ARGV[0] : 'OP_PerlDemo';
+my $dsn = "dbi:SQLite:$dbname";
+my $user = "";
+my $pass = "";
+
+my $dbh = DBI->connect($dsn, $user, $pass, {
+  PrintError => 0,
+  RaiseError => 1,
+  AutoCommit => 1,
+  FetchHashKeyName => 'NAME_lc'
+});
+
+# Create new table with sample data if no dbname argument provided
+unless (defined $ARGV[0]) {
+  # Create table `people`
+  my $sql = '
+  CREATE TABLE IF NOT EXISTS people (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    home VARCHAR(100)
+  ) ';
+
+  $dbh->do($sql);
+
+  # Insert rows from `example_data`
+  for my $person_hash (@example_data) {
+    my @columns = keys %$person_hash;
+    my @values = values %$person_hash;
+
+    local $" = "', '";
+    $sql = "INSERT INTO people ('@columns') VALUES ('@values')";
+
+    $dbh->do($sql);
+  }
+}
+
+# Read all table data
+my $sth = $dbh->prepare("SELECT * FROM people");
+$sth->execute();
+
+# Output table data
+print "All items in table `people`:\n\n";
+my $count = 0;
+while (my $row = $sth->fetchrow_hashref) {
+  $count++;
+  print "Person $count:\n";
+  print "Name: $row->{first_name} $row->{last_name}\n";
+  print "Home: $row->{home} \n\n";
+}
